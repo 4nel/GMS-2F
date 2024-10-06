da24ae30e86d03ece1c46d8da77ab599c8a27ef2
anel <105921696+4nel@users.noreply.github.com>
2024-10-06 23:49:37 +0200

    Updated hanoi.cpp

--- Rekurzivne funkcije/hanoi.cpp
+++ Rekurzivne funkcije/hanoi.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 void solveHanoiRaw(int n, char starting, char helper, char goal) {
-    if(n==1) {
-        std::cout<<"1: "<<starting<<" -> "<<goal<<'\n';
-        return;
-    }
-
-    solveHanoiRaw(n-1, starting, goal, helper);
-    std::cout<<n<<": "<<starting<<" -> "<<goal<<'\n';
-    solveHanoiRaw(n-1, helper, starting, goal);
+        if(n==1) {
+                std::cout<<"1: "<<starting<<" -> "<<goal<<'\n';
+                return;
+        }
+    
+        solveHanoiRaw(n-1, starting, goal, helper);
+        std::cout<<n<<": "<<starting<<" -> "<<goal<<'\n';
+        solveHanoiRaw(n-1, helper, starting, goal);
 }
 
 void solveHanoi(int n) {
-    solveHanoiRaw(n, 'A', 'B', 'C');
-    return;
+        solveHanoiRaw(n, 'A', 'B', 'C');
+        return;
 }
