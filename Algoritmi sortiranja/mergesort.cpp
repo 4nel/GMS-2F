2f0571d30022fc769307400c00cf9426c78ed4a5
anel <105921696+4nel@users.noreply.github.com>
2024-10-06 23:46:22 +0200

    Updated mergesort.cpp

--- Algoritmi sortiranja/mergesort.cpp
+++ Algoritmi sortiranja/mergesort.cpp
@@ -1,22 +1,22 @@
 
 void MergeSort(int* array, int n) {
-    if(n<=1) return;
+        if(n<=1) return;
 
-    int new_array[n], half=n/2, k=0, pointerLeft=0, pointerRight=half;
+        int new_array[n], half=n/2, k=0, pointerLeft=0, pointerRight=half;
 
-    MergeSort(array, half);
-    MergeSort(array+half, n-half);
-
-    while(pointerLeft<half && pointerRight<n) {
-        if(array[pointerLeft]<=array[pointerRight]) {
-            new_array[k++]=array[pointerLeft++];
-        }else {
-            new_array[k++]=array[pointerRight++];
-        }
-    }
-
-    while(pointerLeft<half) new_array[k++]=array[pointerLeft++];
-    while(pointerRight<n) new_array[k++]=array[pointerRight++];
+        MergeSort(array, half);
+        MergeSort(array+half, n-half);
     
-    for(int i=0; i<n; i++) array[i]=new_array[i];
+        while(pointerLeft<half && pointerRight<n) {
+                if(array[pointerLeft]<=array[pointerRight]) {
+                        new_array[k++]=array[pointerLeft++];
+                }else {
+                        new_array[k++]=array[pointerRight++];
+                }
+        }
+    
+        while(pointerLeft<half) new_array[k++]=array[pointerLeft++];
+        while(pointerRight<n) new_array[k++]=array[pointerRight++];
+        
+        for(int i=0; i<n; i++) array[i]=new_array[i];
 }
